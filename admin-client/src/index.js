62fd566265a3f6b31373e63f4898e3face349549
Lai Ngoc Lam <laingoclam3112@gmail.com>
2024-04-13 10:08:34 +0700

    Change hashrouter to browserrouter

--- admin-client/src/index.js
+++ admin-client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'assets/css/App.css';
-import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { HashRouter, Route, Switch, Redirect, BrowserRouter } from 'react-router-dom';
 import AuthLayout from 'layouts/auth';
 import AdminLayout from 'layouts/admin';
 import RtlLayout from 'layouts/rtl';
@@ -13,14 +13,14 @@ ReactDOM.render(
 	<ChakraProvider theme={theme}>
 		<React.StrictMode>
 			<ThemeEditorProvider>
-				<HashRouter>
+				<BrowserRouter>
 					<Switch>
 						<Route path={`/auth`} component={AuthLayout} />
 						<Route path={`/admin`} component={AdminLayout} />
 						<Route path={`/rtl`} component={RtlLayout} />
 						<Redirect from='/' to='/admin' />
 					</Switch>
-				</HashRouter>
+				</BrowserRouter>
 			</ThemeEditorProvider>
 		</React.StrictMode>
 	</ChakraProvider>,
