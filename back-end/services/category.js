4d2dd49f0b695bd835a7d0f3e5227abe92458777
vanquang2002 <132914620+vanquang2002@users.noreply.github.com>
2024-04-22 22:02:06 +0700

    Update category.js

--- back-end/services/category.js
+++ back-end/services/category.js
@@ -1,1 +1,41 @@
-export default {}
+import Category from "../models/Category.js";
+
+export const createCategory = async (categoryData) => {
+    try {
+        return await Category.create(categoryData);
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+export const getAllCategories = async () => {
+    try {
+        return await Category.find();
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+export const getCategoryById = async (categoryId) => {
+    try {
+        return await Category.findById(categoryId);
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+export const updateCategory = async (categoryId, categoryData) => {
+    try {
+        return await Category.findByIdAndUpdate(categoryId, categoryData, { new: true });
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+export const deleteCategory = async (categoryId) => {
+    try {
+        return await Category.findByIdAndDelete(categoryId);
+    } catch (error) {
+        throw new Error(error);
+    }
+};
