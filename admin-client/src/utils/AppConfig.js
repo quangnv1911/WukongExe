8cb8a343eb78ef16e030b11cd39654e41458094d
Lai Ngoc Lam <laingoclam3112@gmail.com>
2024-04-22 23:24:57 +0700

    deploy

--- admin-client/src/utils/AppConfig.js
+++ admin-client/src/utils/AppConfig.js
@@ -1,2 +1,2 @@
-export const BACK_END_HOST = "https://bearpostore.onrender.com";
+export const BACK_END_HOST = "https://bearpostore.onrender.com/api/v1";
 export const FRONT_END_HOST = "http://localhost:5173/";
