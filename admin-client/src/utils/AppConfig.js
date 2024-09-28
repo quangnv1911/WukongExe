8a36c695ea7e17da9101e1a584c25937d869475d
Nguyễn Vinh Quang <112387320+quangnv1911@users.noreply.github.com>
2024-09-28 22:50:31 +0700

    update env

--- admin-client/src/utils/AppConfig.js
+++ admin-client/src/utils/AppConfig.js
@@ -1,6 +1,6 @@
-export const BACK_END_HOST = "https://api.wukongfood.site/api/v1"; // Sử dụng HTTPS
-export const FRONT_END_HOST = "https://wukongfood.site/"; // Sử dụng HTTPS
-export const BACK_END_HOST_QUANG = "https://api.wukongfood.site"; // Sử dụng HTTPS
+export const BACK_END_HOST = "https://wukong-exe-be.vercel.app/api/v1"; // Sử dụng HTTPS
+export const FRONT_END_HOST = "https://wukong-exe.vercel.app/"; // Sử dụng HTTPS
+export const BACK_END_HOST_QUANG = "https://wukong-exe-be.vercel.app"; // Sử dụng HTTPS
 // // export const BACK_END_HOST_QUANG = "http://localhost:3008";
 
 // export const BACK_END_HOST = "http://localhost:3008/api/v1";
