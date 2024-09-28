7f9b3069f72bd7499ea0dca1a5d03f35d39cc728
Nguyễn Vinh Quang <112387320+quangnv1911@users.noreply.github.com>
2024-09-28 23:25:37 +0700

    update env

--- front-end/src/utils/AppConfig.js
+++ front-end/src/utils/AppConfig.js
@@ -1,6 +1,6 @@
 export const BACK_END_HOST = "https://wukong-exe-be.vercel.app/api/v1"; // Sử dụng HTTPS
 export const FRONT_END_HOST = "https://wukong-exe.vercel.app/"; // Sử dụng HTTPS
-export const ADDRESS_HOST = "https://wukong-exe-be.vercel.app"; // Sử dụng HTTPS
+export const ADDRESS_HOST = "https://vietnam-administrative-division-json-server-swart.vercel.app/"; // Sử dụng HTTPS
 
 
 // export const BACK_END_HOST = "http://localhost:3008/api/v1";
